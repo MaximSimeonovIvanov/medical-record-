d18d2746f3289f22a8ae79b1e60ab0f3b403b3df
Maxim Ivanov <maxim.s.ivanoff@gmail.com>
2024-12-06 15:07:58 +0200

    deleted medicine files that are not needed

--- src/main/java/com/jws/medical_record/data/MedicineRepository.java
+++
@@ -1,6 +0,0 @@
-package com.jws.medical_record.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedicineRepository extends JpaRepository <Medicine, Long>{
-}
--- src/main/java/com/jws/medical_record/service/MedicineService.java
+++
@@ -1,6 +0,0 @@
-package com.jws.medical_record.service;
-
-public interface MedicineService {
-    //pyrvata ot crud operaciite za vrashtane na danni, za da vidim kakci sa lekarstvata
-    List<Medicine> getMedicines();
-}
